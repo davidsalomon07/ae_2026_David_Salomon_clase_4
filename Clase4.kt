c2815f9c0e31819e37d7c3dac8e3d026598a70e5
David Salomón <ldsalomon@puce.edu.ec>
2026-05-16 10:35:02 -0500

    Primer commit

---
+++ Clase4.kt
@@ -0,0 +1,15 @@
+// ae_2026_David_Salomon_clase_4_main.kt
+// estructura del commit clase_4 {{valor incremental}}
+fun main() {
+    //Siempre debo tipar mis variables (o constantes)
+    val mensaje: String = 'Hola mundo'
+    var variableMutable: Int = 5
+    println(mensaje)
+    println(variableMutable)
+    
+    variableMutable = 10
+    println(variableMutable)
+    
+    mensaje = "Hola mundo mutado"
+    println(mensaje)
+}
