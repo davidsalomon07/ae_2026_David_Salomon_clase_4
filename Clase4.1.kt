03d73b4b9d1eb896c5cd619037d27300c50c79d8
David Salomón <ldsalomon@puce.edu.ec>
2026-05-16 10:40:57 -0500

    Segundo commit

---
+++ Clase4.1.kt
@@ -0,0 +1,12 @@
+// ae_2026_David_Salomon_clase_4_main.kt
+// estructura del commit clase_4.1 {{valor incremental}}
+fun main() {
+    val name: String = 'Ana'
+    val email: String = 'ana@gmail.com'
+    val number: Int = 67
+    // Ana - ana@gmail.com
+    
+    println(name + ' - ' + email)
+    println('$name - $email')
+    println('Te faltan ${100 - number} para llegar a 100')
+}
